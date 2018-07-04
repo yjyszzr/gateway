0f3fe8035fb4991a9793b8fce8043c288092a510
yjyszzr <1053195207@qq.com>
2018-07-04 17:46:02 +0800

    测试调整用户toke过期时间
    Change-Id: I9fd7b0477e252f4c74f38ce0a07073c81d0e9fe7
    Signed-off-by: yjyszzr <1053195207@qq.com>

--- dl-gate/dl-gate-server/src/main/java/com/dl/gate/filter/DlAuthFilter.java
+++ dl-gate/dl-gate-server/src/main/java/com/dl/gate/filter/DlAuthFilter.java
@@ -57,7 +57,7 @@ public class DlAuthFilter extends ZuulFilter {
     @Autowired
     private GateConfig gateConfig;
 
-    private static final long MAX_TIME = 60L * 1000L;
+    private static final long MAX_TIME = 10L * 1000L;
 
     @Override
     public String filterType() {
