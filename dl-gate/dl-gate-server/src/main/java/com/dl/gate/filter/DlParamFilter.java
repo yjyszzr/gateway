6d39449d1ec3d9b1b795e26986c122ee7871951e
wenhuaiteng <wenhuaiteng@126.com>
2018-12-06 15:11:56 +0800

    device参数

--- dl-gate/dl-gate-server/src/main/java/com/dl/gate/filter/DlParamFilter.java
+++ dl-gate/dl-gate-server/src/main/java/com/dl/gate/filter/DlParamFilter.java
@@ -105,7 +105,7 @@ public class DlParamFilter extends ZuulFilter {
                 Address address = header.toJavaObject(Address.class);
                 ctx.addZuulRequestHeader(CommonConstants.HTTP_HEADER_ADDRESS, JSONHelper.bean2json(address));
                 UserDeviceInfo deviceInfo = JSON.parseObject(header.toJSONString(), UserDeviceInfo.class);
-                ctx.addZuulRequestHeader(CommonConstants.HTTP_HEADER_DEVICE, header);
+                ctx.addZuulRequestHeader(CommonConstants.HTTP_HEADER_DEVICE, header.toJSONString());
                 log.info("[run]" + " result -> " + JSON.toJSONString(deviceInfo));
             }
         } catch (Exception e) {
